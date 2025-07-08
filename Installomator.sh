00429cf7e9d11187f9670d116c89d7e096096438
Michal <michal@gawronskikot.com>
2025-07-08 18:11:36 +0100

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -348,8 +348,8 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
         rosetta2=no
     fi
 fi
-VERSION="LB10.10"
-VERSIONDATE="2025-06-24"
+VERSION="LB10.9"
+VERSIONDATE="2025-07-06"
 
 # MARK: Functions
 
